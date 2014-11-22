d897c306d7367acc88b0f615d23d0df70576ac37
Keegan Parker <keegandparker@gmail.com>
2014-11-21 23:41:26 -0500

    Yet YET Another attempt to fix heroku

--- elo_ladder/urls.py
+++ elo_ladder/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 	url(r'^players/(?P<player_id>\d+)/$', views.player_details, name='player_details'),
  	url(r'^register/$', views.register, name='register'),
   	url(r'^login/$', views.user_login, name='login'),
-  	url(r'^logout/$', views.user_logout, name='logout'),
+  	url(r'^logout/$', views.user_logout, name='logout')
 )
